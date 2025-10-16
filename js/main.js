16f9c0df6c3a17140d410ebb0d5fee44ec7f6e92
Ernesto Novillo <ernesto.novillo@gmail.com>
2025-10-16 14:29:39 -0400

    First working version that loads a map.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // Add a simple welcome message to the console
-    console.log('Website loaded successfully!');
 
-    // Get the current year for the footer
-    const footer = document.querySelector('footer p');
-    const currentYear = new Date().getFullYear();
-    footer.innerHTML = `&copy; ${currentYear} My Website. All rights reserved.`;
-});
+async function initMap() {
+    // Request libraries when needed, not in the script tag.
+    const { Map } = await google.maps.importLibrary("maps");
+
+    const position = { lat: 44.74979194815116, lng: -79.8512010048997 };    // Wye Heritage Marina
+
+    // The map, centered at position
+    let map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: position,
+        mapId: "cf429fad5670f355c2f94461",
+        disableDefaultUI: true,
+    });
+}
+
+initMap();
