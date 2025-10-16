ef06a946d20052bf42769c1dd19d1eee8897bc08
Ernesto Novillo <ernesto.novillo@gmail.com>
2025-10-16 16:55:37 -0400

    WIP: Get data periodically.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const pointCount = 0;
 
 async function initMap() {
     // Request libraries when needed, not in the script tag.
@@ -6,7 +7,7 @@ async function initMap() {
     const position = { lat: 44.74979194815116, lng: -79.8512010048997 };    // Wye Heritage Marina
 
     // The map, centered at position
-    let map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
         center: position,
         mapId: "cf429fad5670f355c2f94461",
@@ -14,4 +15,47 @@ async function initMap() {
     });
 }
 
+
+async function fetchAndProcessJsonFile(url, callback) {
+    try {
+        const urlObject = new URL(url, window.location.href);
+        console.log("Fetching URL:", urlObject.href);
+        const response = await fetch(urlObject.href);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        // console.log("JSON data:", jsonData);
+
+        callback(jsonData);
+    } catch (error) {
+        console.error("Error fetching or processing JSON file:", error);
+    }
+}
+
+
+function getNewData() {
+    fetchAndProcessJsonFile('data/files.json', (data) => {
+        if (data.files && data.files.length > 0) {
+            const latestFile = data.files.at(-1);
+            console.log("Latest file:", latestFile);
+            fetchAndProcessJsonFile(`data/${latestFile}`, (jsonData) => {
+                if (jsonData.points && jsonData.points.length > pointCount) {
+                    pointCount = jsonData.points.length;
+                    console.log(`New points added. Total points: ${pointCount}`);
+                } else {
+                    console.log("No new points added.");
+                }
+            });
+        } else {
+            console.log("No files found in files.json");
+        }
+    });
+}
+
+
 initMap();
+// const intervalId = setInterval(getNewData, 5000);
+getNewData();
