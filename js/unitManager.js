da7ed6b5f525b2c4d0c53c2713ce56d8ecf6dc20
Ernesto Novillo <ernesto.novillo@gmail.com>
2025-11-05 16:24:46 -0500

    Removed space character from unit string.

--- js/unitManager.js
+++ js/unitManager.js
@@ -12,20 +12,20 @@ export class UnitManager {
 		switch (key) {
 			case 'Depth':
 				// Depth in meters -> feet. Use '--' for sentinel large values.
-				return { value: value < 42000000 ? (value * 3.28084).toFixed(value > 3 ? 0 : 1) : '--', unit: ' ft' };
+				return { value: value < 42000000 ? (value * 3.28084).toFixed(value > 3 ? 0 : 1) : '--', unit: 'ft' };
 			case 'AWA':
 				// Apparent wind angle (radians) -> degrees, indicate side
 				return { value: (Math.abs(value) * (180 / Math.PI)).toFixed(0), unit: `° ${value < 0 ? 'port' : 'starboard'}` };
 			case 'AWS':
 			case 'SOG':
 				// Speed in m/s -> knots
-				return { value: (value * 1.94384).toFixed(1), unit: ' knots' };
+				return { value: (value * 1.94384).toFixed(value === 0 ? 0 : value < 10 ? 1 : 0), unit: 'knots' };
 			case 'COG':
 				// Course over ground in radians -> degrees True
 				return { value: (value * (180 / Math.PI)).toFixed(0), unit: '° T' };
 			case 'Distance':
 				// Meters -> nautical miles
-				return { value: (value * 0.000539957).toFixed(1), unit: ' nm' };
+				return { value: (value * 0.000539957).toFixed(1), unit: 'nm' };
 			default:
 				return { value: value, unit: '' };
 		}
